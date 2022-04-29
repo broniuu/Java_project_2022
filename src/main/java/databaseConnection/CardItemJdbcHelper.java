a4b40495a9c27b4d909643a192e24e201ea1ea1f
broniuu <86781217+broniuu@users.noreply.github.com>
2022-04-29 16:04:00 +0200

    make helpers

--- src/main/java/databaseConnection/CardItemJdbcHelper.java
+++ src/main/java/databaseConnection/CardItemJdbcHelper.java
@@ -1,2 +1,74 @@
-package databaseConnection;public class CardItemJdbcHelper {
+package databaseConnection;
+
+import model.CartItem;
+import model.Restaurant;
+import model.User;
+import model.UserComparator;
+
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+
+public class CardItemJdbcHelper {
+    public static final String SHOPPING_CART_TABLE = "ShoppingCart";
+    public static final String COLUMN_CART_ITEM_ID = "CartItemId";
+    public static final String COLUMN_DISH_ID = "DishId";
+    public static final String COLUMN_USER_ID = "UserId";
+
+    DbConnector dbConnector = new DbConnector();
+
+    public List<CartItem> getCartItems(){
+        List<CartItem> cartItems = new ArrayList<>();
+        String queryString = "SELECT * FROM " + SHOPPING_CART_TABLE;
+        try (Statement stmt = this.dbConnector.getConnection().createStatement()) {
+            ResultSet rs = stmt.executeQuery(queryString);
+            while (rs.next()) {
+                int cartItemId = rs.getInt(COLUMN_CART_ITEM_ID );
+                Integer userId = rs.getInt(COLUMN_USER_ID);
+                Integer dishId = rs.getInt(COLUMN_DISH_ID );
+
+                CartItem cartItem = new CartItem(cartItemId, dishId, userId);
+                cartItems.add(cartItem);
+            }
+            dbConnector.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return cartItems;
+    }
+
+    public boolean addCartItem(CartItem cartItem) {
+
+        DbConnector dbConnector = new DbConnector();
+        String queryString = "INSERT INTO "+ SHOPPING_CART_TABLE + " (" +
+                COLUMN_CART_ITEM_ID + ", " +
+                COLUMN_DISH_ID + ", " +
+                COLUMN_USER_ID + " " +
+                ") VALUES (?, ?, ?)";
+        try (PreparedStatement stmt = dbConnector.getConnection().prepareStatement(queryString)) {
+            stmt.setInt(2, cartItem.getUserId());
+            stmt.setInt(3, cartItem.getDishId());
+            stmt.executeUpdate();
+            return true;
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+            return false;
+        }
+    }
+
+    public boolean deleteUser (CartItem cartItem){
+        DbConnector dbConnector = new DbConnector();
+        String queryString = "DELETE FROM " + SHOPPING_CART_TABLE + " WHERE " + COLUMN_CART_ITEM_ID + " = ?";
+        try (PreparedStatement stmt = dbConnector.getConnection().prepareStatement(queryString)) {
+            stmt.setInt(1, cartItem.getCartItemId());
+            stmt.executeUpdate();
+            return true;
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+            return false;
+        }
+    }
 }
--- src/main/java/databaseConnection/Test.java
+++ src/main/java/databaseConnection/Test.java
@@ -1,22 +1,32 @@
 package databaseConnection;
 
-import model.Dish;
-import model.User;
+import model.*;
 
 import java.util.List;
 
 public class Test {
     public static void main(String[] args) {
-        UserJdbcHelper userJdbcHelper = new UserJdbcHelper();
-        List<User> users = userJdbcHelper.getUsers();
-        DishJdbcHelper dishJdbcHelper = new DishJdbcHelper();
-        List<Dish> dishes = dishJdbcHelper.getDishes();
-        User user = new User("login123", "hasło123", "Filip",
-                "Broniek", "Różana 20", "1234567890",
-                "12/25", "111");
-        User user1 = new User(1,"login123", "hasło123", "Filip",
-                "Broniek", "Różana 20", "1234567890",
-                "12/25", "111");
-        boolean success = userJdbcHelper.addUser(user);
+//        UserJdbcHelper userJdbcHelper = new UserJdbcHelper();
+//        List<User> users = userJdbcHelper.getUsers();
+//        DishJdbcHelper dishJdbcHelper = new DishJdbcHelper();
+//        List<Dish> dishes = dishJdbcHelper.getDishes();
+//        RestaurantJdbcHelper restaurantJdbcHelper = new RestaurantJdbcHelper();
+//        List<Restaurant> restaurants = restaurantJdbcHelper.getDRestaurants();
+//        User user = new User("login123", "hasło123", "Filip",
+//                "Broniek", "Różana 20", "1234567890",
+//                "12/25", "111","hehe@himaj.com");
+//        User user1 = new User(1,"login123", "hasło123", "Filip",
+//                "Broniek", "Różana 20", "1234567890",
+//                "12/25", "111","hehe@himaj.com");
+//        boolean delSuccess = userJdbcHelper.deleteUser(user1);
+//        boolean AddSuccess = userJdbcHelper.addUser(user);
+//        List<User> users2 = userJdbcHelper.getUsers();
+
+        CardItemJdbcHelper cardItemJdbcHelper = new CardItemJdbcHelper();
+        CartItem cartItem =new CartItem(1,3);
+        CartItem cartItem2 =new CartItem(2,1,3);
+        //boolean addItemSuccess = cardItemJdbcHelper.addCartItem(cartItem);
+        //boolean delItemSuccess = cardItemJdbcHelper.deleteUser(cartItem2);
+        List<CartItem> cartItems = cardItemJdbcHelper.getCartItems();
     }
 }
